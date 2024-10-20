42f1c52e7390063d2f283ee5513a805636b3e794
Suresh Kumar <suresh05122000@gmail.com>
2024-10-20 08:43:44 +0530

    Print first statement

--- Setup-01/src/main/java/org/example/Main.java
+++ Setup-01/src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//        In java we cannot use single quotes for string, as we use in javascript
+        System.out.println("Hello world!, This is my first step in learning java.");
     }
 }
