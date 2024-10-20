c5d5ac786f7b00eae4990dff2816240785023d36
Suresh Kumar <suresh05122000@gmail.com>
2024-10-20 09:21:13 +0530

    Key words and variables

--- Setup-01/src/main/java/org/example/Main.java
+++ Setup-01/src/main/java/org/example/Main.java
@@ -4,5 +4,9 @@ public class Main {
     public static void main(String[] args) {
 //        In java we cannot use single quotes for string, as we use in javascript
         System.out.println("Hello world!, This is my first step in learning java.");
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        myFirstNumber = 6; // Re-Assign a variable.
+        System.out.println(myFirstNumber);
     }
 }
